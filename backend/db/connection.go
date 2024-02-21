3ae8853d3c617da24be481660937780c6058763d
sardonyx001 <lassouedjamel@hotmail.fr>
2024-02-21 23:34:31 +0900

    feat: add gorm postgres init

---
+++ backend/db/connection.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"backend/config"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init(cfg *config.Config) *gorm.DB {
+	dataSourceName := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		cfg.DB.Host,
+		cfg.DB.User,
+		cfg.DB.Password,
+		cfg.DB.Name,
+		cfg.DB.Port,
+	)
+
+	fmt.Println(dataSourceName)
+
+	db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
