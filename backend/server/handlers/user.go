5e0f4a6db0ea6f35197db29fa481c2cd49392c67
sardonyx001 <lassouedjamel@hotmail.fr>
2024-02-27 16:23:05 +0900

    fix: typo

--- backend/server/handlers/user.go
+++ backend/server/handlers/user.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"backend/auth"
 	"backend/services"
 	"backend/utils"
 	"net/http"
@@ -9,7 +10,7 @@ import (
 )
 
 type (
-	UserHanlder interface {
+	UserHandler interface {
 		GetUserById(c echo.Context) error
 		CreateUser(c echo.Context) error
 		UpdateUserById(c echo.Context) error
@@ -25,14 +26,36 @@ func (h *userHandler) GetUserById(c echo.Context) error {
 	userID := c.Param("id")
 	user, err := h.UserService.GetUserById(userID)
 	if err != nil {
-        
+
 		return c.JSON(http.StatusNotFound, utils.Error{Message: err.Error()})
 	}
 	return c.JSON(http.StatusOK, user)
 }
 
 func (h *userHandler) CreateUser(c echo.Context) error {
-	return c.JSON(http.StatusOK, "CreateUser")
+	userAuth := new(auth.BasicAuth)
+
+	if err := c.Bind(userAuth); err != nil {
+		return c.JSON(http.StatusBadRequest, "Request doesn't match schema")
+	}
+
+	if err := userAuth.Validate(); err != nil {
+		return c.JSON(http.StatusBadRequest, "Required fields are empty or not valid")
+	}
+
+	_, err := h.UserService.GetUserByUsername(userAuth.Username)
+
+	if err == nil {
+		return c.JSON(http.StatusBadRequest, "User already exists")
+	}
+
+	userId, err := h.UserService.CreateUser(userAuth)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Server error occured")
+	}
+
+	return c.JSON(http.StatusCreated, userId)
+
 }
 
 func (h *userHandler) UpdateUserById(c echo.Context) error {
