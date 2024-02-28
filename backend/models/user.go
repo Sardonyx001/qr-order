cb2448f3015a7f05e4098c1f16e11ef4a1c54de4
sardonyx001 <lassouedjamel@hotmail.fr>
2024-02-28 12:30:39 +0900

    refactor: ユーザモデルのStatusフィールドのデフォルト値を設定

--- backend/models/user.go
+++ backend/models/user.go
@@ -13,9 +13,8 @@ type User struct {
 	UUIDBaseModel
 	Username     string        `json:"username"    gorm:"unique; not null"`
 	PasswordHash string        `json:"-"           gorm:"not null"`
-	Status       Status        `json:"status"      gorm:"not null"`
+	Status       Status        `json:"status"      gorm:"default:'ACTIVE';not null"`
 	AdminID      string        `json:"-"`
 	Admin        Admin         `json:"-"`
 	Restaurants  []*Restaurant `json:"restaurants" gorm:"many2many:user_restaurants;"`
-    
 }
