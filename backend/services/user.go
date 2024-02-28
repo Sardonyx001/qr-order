d626f6c2776b186592995bd8da1f8f1ee32408d7
sardonyx001 <lassouedjamel@hotmail.fr>
2024-02-28 14:11:47 +0900

    fix: return missing error

--- backend/services/user.go
+++ backend/services/user.go
@@ -33,7 +33,7 @@ func (s *userService) CreateUser(creds *config.BasicAuth) (string, error) {
 	result := s.stores.DB.First(&defaultAdmin)
 
 	if result.Error != nil {
-		return "", err
+		return "", result.Error
 	}
 
 	user := models.User{
